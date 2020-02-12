5c1cb61b2cb88a4d92995dbf9a80f94233d25b4d
Michael Mair <michael.mair@leftshift.one>
2020-02-12 14:31:19 +0100

    test fix

--- gaia-sdk-java/gaia-sdk-java-atlas/src/test/kotlin/gaia/sdk/atlas/AtlasClientTest.kt
+++ gaia-sdk-java/gaia-sdk-java-atlas/src/test/kotlin/gaia/sdk/atlas/AtlasClientTest.kt
@@ -23,14 +23,14 @@ class AtlasClientTest {
                     lemma()
                     pos()
                     base()
-                    abbreviations { lemma() }
+                    abbreviations()
                     labels()
-                    parts { lemma() }
-                    causations { lemma() }
-                    hyperonyms { lemma() }
-                    meronyms { lemma() }
-                    flexions { lemma() }
-                    synonyms { lemma() }
+                    parts()
+                    causations()
+                    hyperonyms()
+                    meronyms()
+                    flexions()
+                    synonyms()
                 }
                 dep {
                     source()
@@ -113,7 +113,7 @@ class AtlasClientTest {
             val map = ObjectMapper().readValue(payload, Map::class.java)
 
             assertThat(map["variables"]).isEqualTo(mapOf("text1" to "text", "merge1" to false, "normalized1" to true, "qualifier1" to "default"))
-            assertThat(map["statement"]).isEqualTo("query atlas(\$text1: String!, \$merge1: Boolean!, \$qualifier1: String!, \$normalized1: Boolean!) { nlu(text:\$text1, merge:\$merge1) { lex { lemma pos base lex { lemma } labels lex { lemma } lex { lemma } lex { lemma } lex { lemma } lex { lemma } lex { lemma } } dep { source sourceIndex sourcePos sourceBase target targetIndex targetPos targetBase relation main modifier parenthesized } txt cls(qualifier:\$qualifier1) raw ner { datetime { date date1 date2 negation fuzzy indices } duration { amount amountMin amountMax unit negation indices } price { amount amountMin amountMax currency negation indices } bool email url location(normalized:\$normalized1) { name type negation indices } organization { name type negation indices } accommodation { name type negation indices } person { isAdult age negation indices } } } }")
+            assertThat(map["statement"]).isEqualTo("query atlas(\$text1: String!, \$merge1: Boolean!, \$qualifier1: String!, \$normalized1: Boolean!) { nlu(text:\$text1, merge:\$merge1) { lex { lemma pos base abbreviations labels parts causations hyperonyms meronyms flexions synonyms } dep { source sourceIndex sourcePos sourceBase target targetIndex targetPos targetBase relation main modifier parenthesized } txt cls(qualifier:\$qualifier1) raw ner { datetime { date date1 date2 negation fuzzy indices } duration { amount amountMin amountMax unit negation indices } price { amount amountMin amountMax currency negation indices } bool email url location(normalized:\$normalized1) { name type negation indices } organization { name type negation indices } accommodation { name type negation indices } person { isAdult age negation indices } } } }")
             return Publisher { it.onComplete() }
         }
 
