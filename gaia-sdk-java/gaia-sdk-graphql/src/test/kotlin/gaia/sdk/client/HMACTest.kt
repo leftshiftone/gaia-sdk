3524e66a8c53621e9a109683ca7bfc7e5be83042
alberto.geaperez <alberto.geaperez@leftshift.one>
2020-06-23 13:59:15 +0200

    [GAIA-1475] Happy path test to check that the hash function generates always the same result

---
+++ gaia-sdk-java/gaia-sdk-graphql/src/test/kotlin/gaia/sdk/client/HMACTest.kt
@@ -0,0 +1,22 @@
+package gaia.sdk.client
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class HMACTest {
+    lateinit var hmac : HMAC
+
+    @BeforeEach
+    fun setup(){
+        hmac = HMAC("secretKey")
+    }
+
+    @Test
+    fun `generate a HMAC Hash for a byteArray`() {
+        val generatedHash = hmac.hash("HI".toByteArray())
+        val expectedHashAsString= "e0a8cd2bbe082f184138444cdbd0d2ea3dd12d70203973b92722c5505067bf90d5234aae459a148128e4bb5a24dc8410eaa5f56039867a41acc3f6c686e130cf"
+        Assertions.assertThat(String(generatedHash)).isEqualTo(expectedHashAsString)
+    }
+
+}
