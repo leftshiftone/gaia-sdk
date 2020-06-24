9bbc8a06723f6c3d30f503730303fe52660729b0
alberto.geaperez <alberto.geaperez@leftshift.one>
2020-06-24 12:29:01 +0200

    [GAIA-1475] WIP Test

--- gaia-sdk-java/gaia-sdk-graphql/src/test/kotlin/gaia/sdk/client/HMACTest.kt
+++ gaia-sdk-java/gaia-sdk-graphql/src/test/kotlin/gaia/sdk/client/HMACTest.kt
@@ -35,9 +35,20 @@ internal class HMACTest {
         val token = arrayOf("apiKey", Base64.getEncoder().encodeToString(signature), timestamp, nonce).joinToString("_")
         val result = "HMAC-SHA512 $token"
         Assertions.assertThat(result).isEqualTo("HMAC-SHA512 apiKey_MzE5ZjQyNzg3ZTgyZGJhNmE3YTBiNjI5ODA5MjIzMzk2YzRhMTg1MmNlOWUwYzhiYTNiZmQ0MTkxY2NlMDg1YTVlMWM0Y2UwM2QzNzNlM2NhYWIxMzcxMTU5MTQxNTJkNzFhMmEwMmY3OGIwNTZmNjA0NTJkZDJlYzg2ZDE1MjU=_1592924470_353823db-c12b-44b2-b0dc-c4d813c74b24")
-//        val expectedHashAsString= "{\"statement\":\"query gaia($identityId1:Uuid) { retrieve { knowledge { behaviours(identityId:$identityId1){identityId reference} } } }\",\"variables\":{\"identityId1\":\"03e274fe-d835-454b-b2f3-1fd27c3f8aa2\"}}"
-//HMAC-SHA512 apiKey_ZDdjMDNlODI5Yzk4MWUzNjcxNDE0OTlmNzM4MDRmMmFkY2JjMjdiZTUyMjMyMTI4ZTAxZTBlMTFhODQzNWUyMjk5NGE1MzQ2MDQ1ZWRhMzRlZmIzOTc5MmQxNDAwZDQ1MTc2YTcxMzE3ZmEyYjRkNmU1NWFkN2M0ZWM1ZDJkNDI=_1592924386_61a064aa-6719-46c4-9202-7e50fd425555
     }
+
+    //TODO remove this test and create one that checks the token generation
+//    @Test
+//    fun `encoding hi`() {
+//        val encodedString = Base64.getEncoder().encodeToString("hi".toByteArray()) //aGk=
+//        val timestamp = 1592924470//Instant.now().epochSecond
+//        val nonce = "353823db-c12b-44b2-b0dc-c4d813c74b24"//UUID.randomUUID().toString()
+//        val toBeHashed = arrayOf(encodedString, "application/json", "http", timestamp, nonce).joinToString("_")
+////        val signature = Base64.getEncoder().encodeToString(HMAC("secret").hash(toBeHashed.toByteArray()))
+//        val signature = HMAC("secret").hash(toBeHashed.toByteArray())
+//        Assertions.assertThat(String(signature)).isEqualTo("MzE5ZjQyNzg3ZTgyZGJhNmE3YTBiNjI5ODA5MjIzMzk2YzRhMTg1MmNlOWUwYzhiYTNiZmQ0MTkxY2NlMDg1YTVlMWM0Y2UwM2QzNzNlM2NhYWIxMzcxMTU5MTQxNTJkNzFhMmEwMmY3OGIwNTZmNjA0NTJkZDJlYzg2ZDE1MjU=")
+//    }
+
 
 
 }
