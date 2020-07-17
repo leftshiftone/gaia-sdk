db953032b3cd27b306036f0aed47ee0e9cabaf72
leftshiftone-adinauer <62129878+leftshiftone-adinauer@users.noreply.github.com>
2020-07-17 08:05:00 +0200

    [GAIA-1579] Add response body to error message on status code >= 400 (#13)
    * [GAIA-1579] Add response body to error message on status code >= 400
    
    * [GAIA-1579] Test error handling of http transporter
    
    Co-authored-by: aemaem <michael.mair@leftshift.one>

--- gaia-sdk-java/gaia-sdk-http/src/main/kotlin/gaia/sdk/http/HttpTransporter.kt
+++ gaia-sdk-java/gaia-sdk-http/src/main/kotlin/gaia/sdk/http/HttpTransporter.kt
@@ -7,18 +7,14 @@ import gaia.sdk.JWTCredentials
 import gaia.sdk.spi.ClientOptions
 import gaia.sdk.spi.ITransporter
 import io.netty.buffer.Unpooled
-import io.netty.handler.codec.http.HttpResponseStatus
 import org.reactivestreams.Publisher
 import org.slf4j.LoggerFactory
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.netty.http.client.HttpClient
-import reactor.util.function.Tuple2
-import reactor.util.function.Tuples
 import java.io.ByteArrayOutputStream
 import java.time.Instant
 import java.util.*
-import java.util.function.BiFunction
 
 class HttpTransporter(private val url: String, private val httpClient: HttpClient) : ITransporter {
 
@@ -34,39 +30,46 @@ class HttpTransporter(private val url: String, private val httpClient: HttpClien
             log.debug("Payload to send: '${String(bytes)}'")
         }
 
-        return httpClient.headers {
-            it.add("Content-Type", options.contentType)
-            it.add("Authorization", buildAuthorizationHeader(options, String(bytes)))
-        }
+        return httpClient
+                .headers {
+                    it.add("Content-Type", options.contentType)
+                    it.add("Authorization", buildAuthorizationHeader(options, String(bytes)))
+                }
                 .followRedirect(true)
                 .post()
                 .uri(url)
                 .send(Mono.just(Unpooled.copiedBuffer(bytes)))
                 .responseConnection { t, u ->
-                    if (t.status().code() >= 400) {
-                        val tuple = Tuples.of(t.status(), ByteArray(0))
-                        return@responseConnection Flux.just(tuple)
-                    }
-                    val publisher1: Flux<HttpResponseStatus> = Flux.just(t.status())
-                    val publisher2: Flux<ByteArray> = u.inbound().receive().asByteArray().buffer().map { list ->
-                        val bos = ByteArrayOutputStream()
-                        list.forEach(bos::write)
-                        bos.toByteArray()
-                    }
+                    u
+                        .inbound()
+                        .receive()
+                        .asByteArray()
+                        .buffer()
+                        .map { list ->
+                            val bos = ByteArrayOutputStream()
+                            list.forEach(bos::write)
+                            bos.toByteArray()
+                        }
+                        .switchIfEmpty(
+                            Flux.just(t.status())
+                                .flatMap {
+                                    if (it.code() >= 400) {
+                                        val msg = "Error with status code ${t.status().code()} (${t.status().reasonPhrase()}) and no payload"
+                                        Flux.error(HttpTransportException(msg))
+                                    } else {
+                                        Flux.empty<ByteArray>()
+                                    }
+                                }
+                        )
+                        .map { byteArray ->
+                            if (t.status().code() >= 400) {
+                                val msg = "Error with status code ${t.status().code()} (${t.status().reasonPhrase()}) and payload: ${String(byteArray)}"
+                                throw HttpTransportException(msg)
+                            }
 
-                    publisher1.zipWith(publisher2, this.zip())
-                }
-                .flatMap { tuple ->
-                    if (tuple.t1.code() >= 400) {
-                        val msg = "Error with status code ${tuple.t1.code()} (${tuple.t1.reasonPhrase()} and payload ${String(tuple.t2)})"
-                        return@flatMap Flux.error<RuntimeException>(HttpTransportException(msg))
-                    }
-                    return@flatMap Flux.just(jsonparser.readValue(tuple.t2, type))
+                            jsonparser.readValue(byteArray, type)
+                        }
                 }.cast(type)
-    }
-
-    fun zip(): BiFunction<HttpResponseStatus, ByteArray, Tuple2<HttpResponseStatus, ByteArray>> {
-        return BiFunction { a, b -> Tuples.of(a, b) }
     }
 
     private fun buildAuthorizationHeader(options: ClientOptions, payload: String):String {
