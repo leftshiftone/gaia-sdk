ff223747a80b125a4856cc0483a397e1acef978b
rschluesselbauer <raphael.schluesselbauer@leftshift.one>
2020-10-13 18:06:30 +0200

    [GAIA-2263] Fix publishConvInteraction function (#56)
    * [GAIA-2263] Fix publishConvInteraction function
    
    * [GAIA-2263] Add comment

--- gaia-sdk-javascript/src/__tests__/MqttSensorQueue.test.ts
+++ gaia-sdk-javascript/src/__tests__/MqttSensorQueue.test.ts
@@ -1,15 +1,23 @@
-import {MqttSensorQueue, QueueHeader, QueueOptions} from "..";
+import {MqttSensorQueue, QueueHeader, QueueOptions} from '..';
 
-describe.skip("MqttSensorQueue Test", () => {
+describe.skip('MqttSensorQueue Test', () => {
 
     beforeEach(() => {
         jest.setTimeout(10000);
-    })
+    });
 
-    it("connect", () => {
-        const header = new QueueHeader("4a87c137-3894-4580-ae20-8a4f621b75fd", "niceChannel")
-        const mqttSensorQueue = new MqttSensorQueue(new QueueOptions("wss://mqtt.beta.gaia.leftshift.one/mqtt", 443, "heimdall"))
-        mqttSensorQueue.publishConvInteraction(header, {type: "reception", payload: "", attributes: {}})
-    })
-})
+    /**
+     * This test will always be green, because we have no possibility to await the completion of publish.
+     * TODO: Write a meaningful test once we can await the result of publish
+     */
+    it('connect', () => {
+        const header = new QueueHeader('4a87c137-3894-4580-ae20-8a4f621b75fd', 'niceChannel');
+        const mqttSensorQueue = new MqttSensorQueue(new QueueOptions('wss://mqtt.beta.gaia.leftshift.one/mqtt', 443, 'heimdall'));
+        mqttSensorQueue.publishConvInteraction(header, {
+            attributes: {},
+            type: 'reception',
+            payload: {}
+        });
+    });
+});
 
