c4121c72db2ac2e7573104e2d521165a563358df
Alexander Dinauer <alexander.dinauer@leftshift.one>
2020-10-13 11:29:01 +0200

    [GAIA-1732] Fix test (hmac builder now converts to json rather than expecting input to already be converted)

--- gaia-sdk-python/gaia_sdk/http/tests/test_hmactokenbuilder.py
+++ gaia-sdk-python/gaia_sdk/http/tests/test_hmactokenbuilder.py
@@ -12,7 +12,7 @@ class TestHMACTokenBuilder(unittest.TestCase):
         timestamp = int(1592924470)
         payload = "hi"
         nonce = "353823db-c12b-44b2-b0dc-c4d813c74b24"
-        expected_token = "HMAC-SHA512 apiKey_MzE5ZjQyNzg3ZTgyZGJhNmE3YTBiNjI5ODA5MjIzMzk2YzRhMTg1MmNlOWUwYzhiYTNiZmQ0MTkxY2NlMDg1YTVlMWM0Y2UwM2QzNzNlM2NhYWIxMzcxMTU5MTQxNTJkNzFhMmEwMmY3OGIwNTZmNjA0NTJkZDJlYzg2ZDE1MjU=_1592924470_353823db-c12b-44b2-b0dc-c4d813c74b24"
+        expected_token = "HMAC-SHA512 apiKey_ZmQ4ZTE2ZGVlYWIyZjIxODgyN2MxYzY3NWNhZjBkMWYxZjdmZmRkYjRhM2U4MWI5MWVkNzc3M2QzYjkwYmM1MWI2YTU1NzY1Y2EzZmRkMTNiZjgyZDY0NzJiZmFkZGM0NTM1MGY1NzdiYmNjNzM3YzZmMzczZTZmMGM0ZWExMjE=_1592924470_353823db-c12b-44b2-b0dc-c4d813c74b24"
         token = HMACTokenBuilder().with_payload(payload) \
             .with_nonce(nonce) \
             .with_timestamp(timestamp) \
