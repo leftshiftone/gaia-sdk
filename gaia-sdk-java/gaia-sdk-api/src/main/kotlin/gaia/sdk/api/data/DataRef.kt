ccc1ed53fb077e4e7f83232d67d2edbc14f0d74c
Raphael Schluesselbauer <raphael.schluesselbauer@leftshift.one>
2020-11-16 17:30:10 +0100

    [INFRASTRUCTURE] Data API download close file in case of error

--- gaia-sdk-java/gaia-sdk-api/src/main/kotlin/gaia/sdk/api/data/DataRef.kt
+++ gaia-sdk-java/gaia-sdk-api/src/main/kotlin/gaia/sdk/api/data/DataRef.kt
@@ -88,96 +88,102 @@ class DataRef(private val uri: String, private val client: GaiaStreamClient) {
      * @return Publisher of the written file.
      */
     fun asFile(filePath: String = "SDK-DataRef.asFile-${System.currentTimeMillis()}"): Publisher<File> {
-        log.info("Download file from $this.uri to ${filePath}")
-        return this.client.streamBytes(BinaryReadImpulse(this.uri), "/data/source")
-                .observeOn(Schedulers.io())
-                .reduce(FileOutputStream(filePath), { result: FileOutputStream, nextBytes: ByteArray ->
-                    result.write(nextBytes)
-                    result
-                })
-                .map { fos ->
-                    fos.close()
-                    File(filePath)
-                }.toFlowable()
-    }
-        /**
-         * It downloads a file from the DataStorage. It streams all bytes
-         * @return Publisher of the byteArrays that are part of the content of the uri
-         */
-        fun asStream(): Publisher<ByteArray> {
-            log.info("Download bytes from $this.uri")
-            return this.client.streamBytes(BinaryReadImpulse(this.uri), "/data/source")
-        }
+        log.info("Download file from ${this.uri} to $filePath")
+        return Flowable.fromCallable { FileOutputStream(filePath) }
+                .flatMap { downloadToStream(it, filePath) }
+                .doOnComplete { log.info("Download of file from ${this.uri} to $filePath successful") }
     }
 
-    class DataUpload(private val uri: String, private val content: File, private val totalNumberOfChunks: Long, private val override: Boolean) {
-        companion object {
-            private val CHUNK_SIZE: Int = 1024 * 1024 * 5
-            private val log: Logger = LoggerFactory.getLogger(DataUpload::class.java)
-
-            fun create(uri: String, content: File, override: Boolean = false): DataUpload {
-                val numberOfChunks = ceil(content.length().toDouble().div(CHUNK_SIZE.toDouble())).toLong()
-                log.info("Upload will be chunked in $numberOfChunks chunks of size: $CHUNK_SIZE")
-                return DataUpload(uri, content, numberOfChunks, override)
-            }
-        }
-
-        fun execute(client: GaiaStreamClient): Publisher<DataRef> {
-            return Flowable.fromPublisher(initUpload(client))
-                    .doOnNext { log.debug("Data uploaded initiated. UploadId ${it.uploadId}") }
-                    .map { response -> response.uploadId }
-                    .flatMap { uploadId ->
-                        this.uploadChunks(uploadId, client)
-                                .toList()
-                                .toFlowable()
-                                .flatMap { chunkIds ->
-                                    completeUpload(uploadId, chunkIds, client)
-                                            .map { DataRef(this.uri, client) }
-                                }
-                    }
-        }
-
-        private fun initUpload(client: GaiaStreamClient) = client.post(InitBinaryWriteImpulse(this.uri, this.totalNumberOfChunks, this.content.length(), this.override), DataUploadResponse::class.java, "/data/sink/init")
-
-        private fun uploadChunks(uploadId: String, client: GaiaStreamClient): Flowable<ChunkResponse> {
-            val fileChunkIterator = this.content.chunkedSequence(CHUNK_SIZE).iterator()
-            return Flowable.fromIterable(ChunkIterable(fileChunkIterator.withIndex()))
+    private fun downloadToStream(fileOutputStream: FileOutputStream, filePath: String) =
+            this.client.streamBytes(BinaryReadImpulse(this.uri), "/data/source")
                     .observeOn(Schedulers.io())
-                    .map { sendChunk(it, uploadId, client) }
-        }
+                    .reduce(fileOutputStream, { result: FileOutputStream, nextBytes: ByteArray ->
+                        result.write(nextBytes)
+                        result
+                    })
+                    .map { File(filePath) }
+                    .doOnError { log.error("Download of file at ${this.uri} to $filePath failed.") }
+                    .doFinally { fileOutputStream.close() }
+                    .toFlowable()
 
-        private fun sendChunk(it: IndexedValue<ByteArray>, uploadId: String, client: GaiaStreamClient): ChunkResponse {
-            val chunk = BinaryWriteChunkImpulse(uri, uploadId, it.index.toLong() + 1, it.value.size.toLong(), it.value)
-            return Flowable.fromPublisher(client.post(chunk.chunk, DataUploadChunkResponse::class.java, "/data/sink/chunk", "application/octet-stream", chunk.requestParameters()))
-                    .map { ChunkResponse(chunk.ordinal, it) }
-                    .doOnNext { log.debug("Chunk number ${it.ordinal} was sent and response ${it.res} was received") }.blockingFirst()
-        }
+    /**
+     * It downloads a file from the DataStorage. It streams all bytes
+     * @return Publisher of the byteArrays that are part of the content of the uri
+     */
+    fun asStream(): Publisher<ByteArray> {
+        log.info("Download bytes from $this.uri")
+        return this.client.streamBytes(BinaryReadImpulse(this.uri), "/data/source")
+    }
+}
 
-        private fun completeUpload(uploadId: String, chunkIds: List<ChunkResponse>, client: GaiaStreamClient) = Flowable.fromPublisher(
-                client.post(CompleteBinaryWriteImpulse(this.uri, uploadId, chunkIds.sortedBy { it.ordinal }.map { it.res.chunkId }), DataUploadResponse::class.java, "/data/sink/complete"))
-                .doOnError { reason ->
-                    val msg = "Upload to uri " + this.uri + " failed: " + reason.message
-                    log.error(msg)
-                    throw RuntimeException(msg)
+class DataUpload(private val uri: String, private val content: File, private val totalNumberOfChunks: Long, private val override: Boolean) {
+    companion object {
+        private val CHUNK_SIZE: Int = 1024 * 1024 * 5
+        private val log: Logger = LoggerFactory.getLogger(DataUpload::class.java)
+
+        fun create(uri: String, content: File, override: Boolean = false): DataUpload {
+            val numberOfChunks = ceil(content.length().toDouble().div(CHUNK_SIZE.toDouble())).toLong()
+            log.info("Upload will be chunked in $numberOfChunks chunks of size: $CHUNK_SIZE")
+            return DataUpload(uri, content, numberOfChunks, override)
+        }
+    }
+
+    fun execute(client: GaiaStreamClient): Publisher<DataRef> {
+        return Flowable.fromPublisher(initUpload(client))
+                .doOnNext { log.debug("Data uploaded initiated. UploadId ${it.uploadId}") }
+                .map { response -> response.uploadId }
+                .flatMap { uploadId ->
+                    this.uploadChunks(uploadId, client)
+                            .toList()
+                            .toFlowable()
+                            .flatMap { chunkIds ->
+                                completeUpload(uploadId, chunkIds, client)
+                                        .map { DataRef(this.uri, client) }
+                            }
                 }
-
     }
 
-    class ChunkIterable(val iterator: Iterator<IndexedValue<ByteArray>>) : Iterable<IndexedValue<ByteArray>> {
-        override fun iterator(): Iterator<IndexedValue<ByteArray>> = iterator
+    private fun initUpload(client: GaiaStreamClient) = client.post(InitBinaryWriteImpulse(this.uri, this.totalNumberOfChunks, this.content.length(), this.override), DataUploadResponse::class.java, "/data/sink/init")
+
+    private fun uploadChunks(uploadId: String, client: GaiaStreamClient): Flowable<ChunkResponse> {
+        val fileChunkIterator = this.content.chunkedSequence(CHUNK_SIZE).iterator()
+        return Flowable.fromIterable(ChunkIterable(fileChunkIterator.withIndex()))
+                .observeOn(Schedulers.io())
+                .map { sendChunk(it, uploadId, client) }
     }
 
-    data class ChunkResponse(val ordinal: Long, val res: DataUploadChunkResponse)
+    private fun sendChunk(it: IndexedValue<ByteArray>, uploadId: String, client: GaiaStreamClient): ChunkResponse {
+        val chunk = BinaryWriteChunkImpulse(uri, uploadId, it.index.toLong() + 1, it.value.size.toLong(), it.value)
+        return Flowable.fromPublisher(client.post(chunk.chunk, DataUploadChunkResponse::class.java, "/data/sink/chunk", "application/octet-stream", chunk.requestParameters()))
+                .map { ChunkResponse(chunk.ordinal, it) }
+                .doOnNext { log.debug("Chunk number ${it.ordinal} was sent and response ${it.res} was received") }.blockingFirst()
+    }
 
-    fun File.chunkedSequence(chunk: Int): Sequence<ByteArray> {
-        val input = this.inputStream().buffered()
-        val buffer = ByteArray(chunk)
-        return generateSequence {
-            val red = input.read(buffer)
-            if (red >= 0) buffer.copyOf(red)
-            else {
-                input.close()
-                null
+    private fun completeUpload(uploadId: String, chunkIds: List<ChunkResponse>, client: GaiaStreamClient) = Flowable.fromPublisher(
+            client.post(CompleteBinaryWriteImpulse(this.uri, uploadId, chunkIds.sortedBy { it.ordinal }.map { it.res.chunkId }), DataUploadResponse::class.java, "/data/sink/complete"))
+            .doOnError { reason ->
+                val msg = "Upload to uri " + this.uri + " failed: " + reason.message
+                log.error(msg)
+                throw RuntimeException(msg)
             }
+
+}
+
+class ChunkIterable(val iterator: Iterator<IndexedValue<ByteArray>>) : Iterable<IndexedValue<ByteArray>> {
+    override fun iterator(): Iterator<IndexedValue<ByteArray>> = iterator
+}
+
+data class ChunkResponse(val ordinal: Long, val res: DataUploadChunkResponse)
+
+fun File.chunkedSequence(chunk: Int): Sequence<ByteArray> {
+    val input = this.inputStream().buffered()
+    val buffer = ByteArray(chunk)
+    return generateSequence {
+        val red = input.read(buffer)
+        if (red >= 0) buffer.copyOf(red)
+        else {
+            input.close()
+            null
         }
     }
+}
