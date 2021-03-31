b105c1910730cfe9feef0e95fa23cb48ee981139
fridayy <benjamin.krenn@leftshift.one>
2021-03-31 17:34:21 +0200

    ....

--- gaia-sdk-javascript/src/__tests__/Introspect.test.ts
+++ gaia-sdk-javascript/src/__tests__/Introspect.test.ts
@@ -3,28 +3,28 @@ import {Mock} from '../mock/mock';
 describe("Introspection Suite", () => {
 
     test("get jobs smoke", () => {
-        return new Promise((resolve, reject) => {
-            const gaiaRef = Mock.gaiaRef(() => JSON.stringify({
-                                                                  "data": {
-                                                                      "introspect": {
-                                                                          "buildJobs": [
-                                                                              {
-                                                                                  "reference": "47de74f7-3dd3-34ca-9b92-3690a2f8cc91",
-                                                                                  "name": "skill-omdb-v0.1.3-build",
-                                                                                  "tenantId": "f000f000-f000-f000-f000-f000f000f000",
-                                                                                  "tag": "v0.1.3",
-                                                                                  "status": {
-                                                                                      "failures": [],
-                                                                                      "running": 1,
-                                                                                      "pending": 0,
-                                                                                      "health": "BUILDING"
-                                                                                  }
+        const gaiaRef = Mock.gaiaRef(() => JSON.stringify({
+                                                              "data": {
+                                                                  "introspect": {
+                                                                      "buildJobs": [
+                                                                          {
+                                                                              "reference": "47de74f7-3dd3-34ca-9b92-3690a2f8cc91",
+                                                                              "name": "skill-omdb-v0.1.3-build",
+                                                                              "tenantId": "f000f000-f000-f000-f000-f000f000f000",
+                                                                              "tag": "v0.1.3",
+                                                                              "status": {
+                                                                                  "failures": [],
+                                                                                  "running": 1,
+                                                                                  "pending": 0,
+                                                                                  "health": "BUILDING"
                                                                               }
-                                                                          ]
-                                                                      }
+                                                                          }
+                                                                      ]
                                                                   }
-                                                              })
-            );
+                                                              }
+                                                          })
+        );
+        return new Promise((resolve, reject) => {
             gaiaRef.introspectBuildJobs("f000f000-f000-f000-f000-f000f000f000")
                 .subscribe(n => {
                     //@ts-ignore
--- gaia-sdk-javascript/src/__tests__/Practice.test.ts
+++ gaia-sdk-javascript/src/__tests__/Practice.test.ts
@@ -4,24 +4,24 @@ import {CancelSkillBuildJobImpulse, CreateSkillBuildJobImpulse} from '../graphql
 describe("Practice Suite", () => {
 
     test("build smoke", () => {
-        return new Promise((resolve, reject) => {
-            const gaiaRef = Mock.gaiaRef(() => JSON.stringify({
-                                                                  "data": {
-                                                                      "practice": {
-                                                                          "build": {
-                                                                              "id": "e2b92d76-b9ac-4626-8a97-fc25f0b79160",
-                                                                              "data": {
-                                                                                  "name": "skill-omdb-v0.1.3-build",
-                                                                                  "reference": "47de74f7-3dd3-34ca-9b92-3690a2f8cc91",
-                                                                                  "tenantId": "f000f000-f000-f000-f000-f000f000f000",
-                                                                                  "skillRef": "a244d3d9-10fb-453a-8f9c-bb7e5e52a619",
-                                                                                  "tag": "v0.1.3"
-                                                                              }
+        const gaiaRef = Mock.gaiaRef(() => JSON.stringify({
+                                                              "data": {
+                                                                  "practice": {
+                                                                      "build": {
+                                                                          "id": "e2b92d76-b9ac-4626-8a97-fc25f0b79160",
+                                                                          "data": {
+                                                                              "name": "skill-omdb-v0.1.3-build",
+                                                                              "reference": "47de74f7-3dd3-34ca-9b92-3690a2f8cc91",
+                                                                              "tenantId": "f000f000-f000-f000-f000-f000f000f000",
+                                                                              "skillRef": "a244d3d9-10fb-453a-8f9c-bb7e5e52a619",
+                                                                              "tag": "v0.1.3"
                                                                           }
                                                                       }
                                                                   }
-                                                              })
-            );
+                                                              }
+                                                          })
+        );
+        return new Promise((resolve, reject) => {
             gaiaRef.practiceBuild(new CreateSkillBuildJobImpulse("f000f000-f000-f000-f000-f000f000f000", "a244d3d9-10fb-453a-8f9c-bb7e5e52a619", "v0.1.3"))
                 .subscribe(n => {
                     expect(n).not.toBeUndefined();
@@ -35,23 +35,23 @@ describe("Practice Suite", () => {
 
 
     test("cancel smoke", () => {
-        return new Promise((resolve, reject) => {
-            const gaiaRef = Mock.gaiaRef(() => JSON.stringify(    {
-                                                                      "data": {
-                                                                          "practice": {
-                                                                              "cancel": {
-                                                                                  "id": "a8181dcd-84f5-45a0-ae77-cb0f89e3d7c2",
-                                                                                  "data": {
-                                                                                      "name": "skill-omdb-v0.1.3-build",
-                                                                                      "reference": "47de74f7-3dd3-34ca-9b92-3690a2f8cc91",
-                                                                                      "tenantId": "f000f000-f000-f000-f000-f000f000f000",
-                                                                                      "tag": "v0.1.3"
-                                                                                  }
+        const gaiaRef = Mock.gaiaRef(() => JSON.stringify(    {
+                                                                  "data": {
+                                                                      "practice": {
+                                                                          "cancel": {
+                                                                              "id": "a8181dcd-84f5-45a0-ae77-cb0f89e3d7c2",
+                                                                              "data": {
+                                                                                  "name": "skill-omdb-v0.1.3-build",
+                                                                                  "reference": "47de74f7-3dd3-34ca-9b92-3690a2f8cc91",
+                                                                                  "tenantId": "f000f000-f000-f000-f000-f000f000f000",
+                                                                                  "tag": "v0.1.3"
                                                                               }
                                                                           }
                                                                       }
-                                                                  })
-            );
+                                                                  }
+                                                              })
+        );
+        return new Promise((resolve, reject) => {
             gaiaRef.practiceCancel(new CancelSkillBuildJobImpulse("f000f000-f000-f000-f000-f000f000f000", "47de74f7-3dd3-34ca-9b92-3690a2f8cc91"))
                 .subscribe(n => {
                     expect(n).not.toBeUndefined();
